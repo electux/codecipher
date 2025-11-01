6a17ef03318ec3a05cc827ad56fd15f57d42651e
Vladimir Roncevic <elektron.ronca@gmail.com>
2025-11-01 08:01:09 +0100

    [codecipher] Update setup.py removed License classifiers, update codecoverage

--- tests/run_coverage.sh
+++ tests/run_coverage.sh
@@ -7,5 +7,11 @@
 # @author  Vladimir Roncevic <elektron.ronca@gmail.com>
 #
 
+rm -rf htmlcov codecipher_coverage.xml codecipher_coverage.json .coverage
 python3 -m coverage run -m --source=../codecipher unittest discover -s ./ -p '*_test.py' -vvv
-python3 -m coverage html
+python3 -m coverage html -d htmlcov
+python3 -m coverage xml -o codecipher_coverage.xml 
+python3 -m coverage json -o codecipher_coverage.json
+python3 -m coverage report --format=markdown -m
+python3 ats_coverage.py -n codecipher
+echo "Done"
